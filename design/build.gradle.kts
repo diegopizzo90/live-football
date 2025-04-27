6b35198b154487fcd0fe8dc6afe272eb22f4e9df
Diego Pizzo <>
2025-04-27 12:12:27 +0100

    migrate design module

--- design/build.gradle.kts
+++ design/build.gradle.kts
@@ -1,29 +1,50 @@
 plugins {
+    id(libs.plugins.kotlin.multiplatform.get().pluginId)
     id(libs.plugins.android.library.get().pluginId)
-    id(libs.plugins.jetbrains.kotlin.android.get().pluginId)
     id(libs.plugins.liveFootball.plugin.get().pluginId)
     id(libs.plugins.compose.compiler.get().pluginId)
 }
 
-android {
-    namespace = "com.diegopizzo.livefootball.design"
+kotlin {
+    androidTarget()
+    iosX64()
+    iosSimulatorArm64()
+    iosArm64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {}
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation(project(":core"))
+                implementation(libs.androidx.core.ktx)
+                implementation(libs.androidx.appcompat)
+
+                // Compose
+                api(project.dependencies.platform(libs.compose.bom))
+                api(libs.compose.coil)
+                api(libs.compose.ui)
+                api(libs.compose.uiTooling.get().toString()) {
+                    exclude(group = "androidx.compose.material", module = "material")
+                }
+                api(libs.compose.design)
+                api(libs.compose.activity)
+                api(libs.compose.lottie)
+                api(libs.compose.haze)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val iosMain by creating {
+            dependencies {}
+        }
+    }
 }
 
-dependencies {
-    implementation(project(path = ":core"))
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-
-    //Compose
-    api(platform(libs.compose.bom))
-    api(libs.compose.coil)
-    api(libs.compose.ui)
-    api(libs.compose.uiTooling) {
-        exclude(group = "androidx.compose.material", module = "material")
-    }
-    api(libs.compose.design)
-    api(libs.compose.activity)
-
-    api(libs.compose.lottie)
-    api(libs.compose.haze)
+android {
+    namespace = "com.diegopizzo.livefootball.design"
 }
